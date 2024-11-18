52976631cbcc68a5629477f8b1a1827f87cf168a
LukasWolff2002 <lwolff@miuandes.cl>
2024-11-18 16:37:30 -0300

    avance

--- CODIGO/codigo.py
+++ CODIGO/codigo.py
@@ -14,9 +14,17 @@ velocidad_crucero = 40 #km/h
 #https://sni.gob.cl/storage/docs/Informe_precios_sociales_2024_SNI-Chile.pdf
 valor_tiempo_viaje = 3114 #$/h
 valor_combustible = 804 #$/L
-costo_movimiento = 81.3 / 1000 #L/km
-costo_detencion = 0.00551 #L/det
+costo_movimiento = 68.9/ 1000 #L/km
+costo_detencion = 8.16/ 1000 #L/det
 consumo_total = 1.2 #L/km
+consumo_lubricante = 1.109 + 0.005498 * rugosidad
+valor_lubricante = 8081
+consumo_neumaticos = 0.052 + 0.001241*rugosidad
+valor_neumaticos = 74114
+consumo_repuestos = 0.173 + 0.00286*(rugosidad**1.87)
+valor_repuestos = 113905965
+consumo_mano_obra = 1.083 + 0.02539*(rugosidad**1.74)
+valor_mano_obra = 6467
 
 #------------------------------------------------------
 #Evaluacion social previo a la mejora y posterior a la mejora
@@ -36,7 +44,8 @@ mejora_propuesta['CTi'] = mejora_propuesta.apply(calcular_cti, axis=1)
 #Para cada arco debo calcular los costos que implica
 def calcular_cc(row):
     S1 = costo_movimiento * 1
-    detenciones = ((row['costo aprox. [h]'] - 1 / velocidad_crucero) * 3600) / duracion_detencion
+    detenciones = ((row['costo aprox. [h]'] - (1 / velocidad_crucero)) * 3600) / duracion_detencion
+    print(detenciones)
     S2 = detenciones * costo_detencion
     demoras = detenciones * duracion_detencion
     S3 = demoras * consumo_total
@@ -44,10 +53,22 @@ def calcular_cc(row):
     CC = (S1 + S2) * row['flujo [veh]'] * valor_combustible * duracion_hora_punta
     return CC
 
-# Aplicar la función a cada fila del DataFrame
 asignacion_por_arcos['CC'] = asignacion_por_arcos.apply(calcular_cc, axis=1)
 mejora_propuesta['CC'] = mejora_propuesta.apply(calcular_cc, axis=1)
 
+def calcular_oc(row):
+   
+    S1 = (consumo_lubricante * valor_lubricante)/1000
+    S2 = (consumo_neumaticos * valor_neumaticos)/1000
+    S3 = (consumo_repuestos * valor_repuestos)/100000
+    S4 = consumo_mano_obra * valor_mano_obra/1000
+
+    OC = (S1 + S2 + S3 + S4) * row['flujo [veh]'] * duracion_hora_punta * 1
+
+    return OC
+
+asignacion_por_arcos['OC'] = asignacion_por_arcos.apply(calcular_oc, axis=1)
+mejora_propuesta['OC'] = mejora_propuesta.apply(calcular_oc, axis=1)
 
 
 
